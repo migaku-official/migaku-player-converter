ff021416f03866f51f9f7b4234656f76852f8ece
Julius Dehner <julius.dehner@gmail.com>
2021-09-20 14:54:19 +0200

    Update build

--- windows-build.sh
+++ windows-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm -rf .venv build dist &&
-        vagrant up &&
-        vagrant winrm --command "cd c:\vagrant ; poetry config virtualenvs.in-project true ; poetry install ; poetry run pyinstaller --onefile --clean --add-binary 'binaries;.' migaku-player-converter.py"
+        vagrant up windows &&
+        vagrant winrm --command "cd c:\vagrant ; poetry config virtualenvs.in-project true ; poetry install ; poetry run pyinstaller --onefile --clean --add-binary 'binaries/windows;.' migaku-player-converter.py"
